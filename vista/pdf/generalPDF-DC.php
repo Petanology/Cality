bcf2780544cd085b16a81d9d14362768d6269e0e
César Suárez <cesaraz455@gmail.com>
2018-11-16 17:02:34 -0500

    Estructura de plantilla pdf en progreso, el header terminado de informe venta directa, y tabla en espera para diseño

--- vista/pdf/generalPDF-DC.php
+++ vista/pdf/generalPDF-DC.php
@@ -1,36 +1,41 @@
 <?php
+
     // importación de la librería
     require_once("fpdf/fpdf.php");
 
     class PDF extends FPDF {
         
         function Header(){
-            
+            // Fondo Azul
             $this->SetFillColor(46,134,193);
             $this->SetXY(0,0);
-            $this->Cell(210,25,'',0,1,'C',1);
+            $this->Cell(216,24,'',0,0,'C',1);
             
+            // Logo GF
             $this->Image('../img/gf-logo.png',10,5,15);
             
-            $this->SetFillColor(255,255,255);
-            $this->Cell(1,20,'',0,1,'C',1);
-            $this->SetXY(20,);
+            // Linea blanca
+            $this->SetDrawColor(230,230,230);
+            $this->line(28,10,28,17);
             
-            //$this->SetFillColor(255,255,255);
-            $this->Ln(20);
+            // Logo Cality
             
-            $this->SetFont('Arial','',10);
-            $this->Cell(30);
-            $this->Cell(120, 10, 'Reporte de Estados', 0 , 0 , '' , 1);
+            $this->Image('../img/faviconx512-4.png',31,7,12);
             
-            $this->Ln(20);
+            $this->SetFillColor(52, 152, 219);
+            $this->SetTextColor(255, 255, 255);
+            $this->SetFont('Arial','B',13);
+            $this->SetXY(50,7);;
+            $this->Cell(0,13,'INFORME VENTA DIRECTA AGOSTO 2018','LR',0,'C',1);
+
+            $this->Ln(25);
         }
         
         
         function Footer(){
             $this->SetY(-15);
             $this->SetFont('Arial', 'B', 7);
-            $this->Cell(0 , 10, utf8_decode('Página ') . $this->PageNo() . '/{nb}', 0 , 0 , 'C' );
+            $this->Cell(0 , 10, 'Página ' . $this->PageNo() . '/{nb}', 0 , 0 , 'C' );
         }
     }
 ?>
--- vista/pdf/reportePromedioContactoDirecto.php
+++ vista/pdf/reportePromedioContactoDirecto.php
@@ -2,12 +2,12 @@
     require_once("../../modelo/ejemploDao.php");
     require_once("generalPDF-DC.php");
     
-    $pdf = new PDF();
+    $pdf = new PDF('P','mm','letter');
     $pdf->AliasNbPages();
     $pdf->AddPage();
 
-
-    $pdf->SetFillColor(232,232,232);
+    $pdf->SetFillColor(52, 152, 219);
+    $pdf->SetDrawColor(155,155,155);
     $pdf->SetFont('Arial','B',12);
     $pdf->Cell(20,6,'ID',1,0,'C',1);
     $pdf->Cell(70,6,'NOMBRE',1,0,'C',1);
@@ -19,36 +19,49 @@
 
     $pdf->SetFont('Arial','',12);
 
+    $filaTabla = 1;
+
     foreach($listar as $rowEJE){
         
+        if($filaTabla % 2 == 0){
+            $pdf->SetFillColor(220,220,220);
+        }else {
+            $pdf->SetFillColor(255,255,255);
+        }
         $pdf->Cell(20,6,$rowEJE[0],1,0,'C',1);
         $pdf->Cell(70,6,$rowEJE[1],1,0,'C',1);
         $pdf->Cell(70,6,$rowEJE[2],1,1,'C',1);
-
+            
+            
+        $filaTabla++;
     }
-    
-    $pdf->Output();
 
     /*
         $pdf->Cell(70,6,"ejemplo",0,0,'C',1);
         $pdf->Cell(70,6,"ejemplo",0,1,'C',1);
-    */
 
-
-
-
-    /*
-    
     foreach($resultado as $row){
         
         $pdf->Cell(20,6,$row[0],1,0,'C',1);
         $pdf->Cell(70,6,$row[1],1,0,'C',1);
         $pdf->Cell(70,6,$row[2],1,1,'C',1);
         
-    }*/
+    }
 
+    */
 
+    $pdf->Close();
+    $pdf->Output("I","informe-venta-directa_agosto-2018.pdf");
 ?>
+
+
+
+
+
+
+
+
+
 
 
 
